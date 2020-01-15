dbd0b29c2f6a5e0051c879f70ddaf47bac4415d6
DPDyakov18 <dpdyakov18@codingburgas.bg>
2020-01-15 19:28:41 +0200

    Added floater code to the main arduino code

--- BlueEconomyArduino.ino
+++ BlueEconomyArduino.ino
@@ -1,6 +1,8 @@
 #include "DHT.h"
 #define DHTTYPE DHT11
 
+int FloatSensor = D3;
+int buttonState = 1;
 const int pResistor = A0;
 int value;
 
@@ -10,16 +12,12 @@ DHT dht(dht_dpin, DHTTYPE);
 void setup(){
   dht.begin();
   Serial.begin(9600);
-  delay(700);
- pinMode(pResistor, INPUT);
-  Serial.begin(9600);
+  pinMode(FloatSensor, INPUT_PULLUP);
+  pinMode(pResistor, INPUT);
 }
 
-
-void loop(){
-  int value = analogRead(pResistor);
-  Serial.println(value);
-
+void tmph()
+{
     float h = dht.readHumidity();
     float t = dht.readTemperature();         
     Serial.print("Current humidity = ");
@@ -29,4 +27,25 @@ void loop(){
     Serial.print(t); 
     Serial.println("C  ");
     delay(800);
+};
+
+void Floater()
+{
+  buttonState = digitalRead(FloatSensor);
+  if (buttonState == HIGH) 
+  {
+    Serial.println("No water");
+  }
+  else
+  {
+    Serial.println("Has water");
+  }
+  delay(100);
+};
+
+void loop(){
+  int value = analogRead(pResistor);
+  Serial.println(value);
+  tmph();
+  Floater();
 }
